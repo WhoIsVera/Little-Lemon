640759c8b29eac8b9f67063dea1ee8d08242fc39
WhoIsVera <ev765030@gmail.com>
2024-11-01 14:37:59 -0600

    Inicializacion de contenido estatico

--- reservation/views.py
+++ reservation/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+#from .models import Menu
+# Create your views here La vista recibe el objeto de solicitud HTTP.
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
+
+
